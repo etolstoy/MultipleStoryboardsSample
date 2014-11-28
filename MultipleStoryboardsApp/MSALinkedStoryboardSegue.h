4d792f32a517085680f6acd25ac98f520cb57ebb
etolstoy <e.tolstoy@rambler-co.ru>
2014-11-28 12:09:23 +0300

    Добавлены все экраны на главную Storyboard

---
+++ MultipleStoryboardsApp/MSALinkedStoryboardSegue.h
@@ -0,0 +1,13 @@
+//
+//  MSALinkedStoryboardSegue.h
+//  MultipleStoryboardsApp
+//
+//  Created by Egor Tolstoy on 11/28/14.
+//  Copyright (c) 2014 etolstoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSALinkedStoryboardSegue : UIStoryboardSegue
+
+@end
---
+++ MultipleStoryboardsApp/MSALinkedStoryboardSegue.m
@@ -0,0 +1,13 @@
+//
+//  MSALinkedStoryboardSegue.m
+//  MultipleStoryboardsApp
+//
+//  Created by Egor Tolstoy on 11/28/14.
+//  Copyright (c) 2014 etolstoy. All rights reserved.
+//
+
+#import "MSALinkedStoryboardSegue.h"
+
+@implementation MSALinkedStoryboardSegue
+
+@end
