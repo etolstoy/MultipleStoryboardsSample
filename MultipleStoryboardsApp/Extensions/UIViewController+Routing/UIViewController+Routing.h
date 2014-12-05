45ce3e484fc761ece12e19070d8942f6599676b9
etolstoy <e.tolstoy@rambler-co.ru>
2014-12-05 15:01:16 +0300

    Задокументирован UIViewController+Routing

--- MultipleStoryboardsApp/Extensions/UIViewController+Routing/UIViewController+Routing.h
+++ MultipleStoryboardsApp/Extensions/UIViewController+Routing/UIViewController+Routing.h
@@ -11,12 +11,29 @@
 
 @interface UIViewController (Routing)
 
+/**
+ *  Абстрактный роутер, в который прокидывается метод prepareForSegue:
+ */
 @property (nonatomic, strong) id<MSARoutingProtocol> router;
 
+/**
+ *  Метод позволяет помимо стандарных identifier и sender передавать словарь с собственной информацией.
+ *
+ *  @param identifier Identifier вызываемой segue
+ *  @param sender     Sender
+ *  @param userInfo   Словарь, содержащий информацию, которую нужно передать в prepareForSegue:
+ */
 - (void)performSegueWithIdentifier:(NSString *)identifier
                             sender:(id)sender
                           userInfo:(NSDictionary *)userInfo;
 
+/**
+ *  Метод, позволяющий получить словарь, переданный с конкретной segue.
+ *
+ *  @param segue Segue, чей userInfo нужно получить
+ *
+ *  @return Словарь с информацией, переданной с segue.
+ */
 - (NSDictionary *)segueUserInfo:(UIStoryboardSegue *)segue;
 
 @end
